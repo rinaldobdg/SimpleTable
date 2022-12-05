931eb8a0c198f03b84cecf3d70398b9ad4adb1be
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-12-05 15:40:27 +0700

    Initial Commit

---
+++ SimpleTable/SimpleTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTableApp.swift
+//  SimpleTable
+//
+//  Created by Benny Rinaldo on 05/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
