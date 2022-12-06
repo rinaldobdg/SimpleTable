8b23a0dad5615cc3e41b0356ec6ab2ea3c1ace8a
Benny Rinaldo <rinaldobdg@yahoo.com>
2022-12-06 09:35:39 +0700

    first commit

--- SimpleTable/ContentView.swift
+++ SimpleTable/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats and Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats and Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            ForEach(restaurantNames.indices, id: \.self) { index in
+                HStack {
+                    Image(restaurantImages[index])
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                    
+                    Text(restaurantNames[index])
+                }
+                
+            }
         }
-        .padding()
+        .listStyle(.plain)
     }
 }
 
